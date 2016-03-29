922a0436cfde2721ab71267bbe29200d020de01b
Adeola Badmus <badmusad@gmail.com>
2016-03-29 23:31:10 +0100

    Complete Question Page Login

--- app/questions/controller.py
+++ app/questions/controller.py
@@ -1,13 +1,16 @@
-from flask import redirect, flash, url_for
+from flask import redirect, flash, url_for, abort
 
 from . import questions
-from ..auth.utils.user import get_logged_in_user
+from ..auth.utils.user import get_logged_in_user, get_user_by_social_id
 
 
 @questions.route('/<int:user_id>/questions/')
 def user_questions(user_id):
-    user_id = get_logged_in_user()
-    if not user_id:
+    _user_id = get_logged_in_user()
+    if not _user_id:
         flash('Kindly log in')
         return redirect(url_for('index'))
+    user = get_user_by_social_id(user_id)
+    if not user:
+        abort(404)
     return 'Welcome, user %s' % user_id
